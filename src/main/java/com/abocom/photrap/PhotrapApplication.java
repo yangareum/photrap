9e6aa813f54282197b11435a32341b8d0ec06cfd
areumyang <yarm76@naver.com>
2021-03-14 18:37:47 +0900

    create develop

---
+++ src/main/java/com/abocom/photrap/PhotrapApplication.java
@@ -0,0 +1,13 @@
+package com.abocom.photrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotrapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abocom/photrap/PhotrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abocom.photrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotrapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
